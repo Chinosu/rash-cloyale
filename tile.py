66906f34067de894fe6955eaae84387392065e54
Megan <46995931+Chinosu@users.noreply.github.com>
2023-12-02 15:58:15 +1100

    Implement tile.py

--- tile.py
+++ tile.py
@@ -2,11 +2,19 @@ import pygame
 
 
 class Tile():
-    def __init__(self, tilepath) -> None:
-        self.tile_image = pygame.image.load(f'assets/images/{tilepath}').convert_alpha()
+    def __init__(self, tile_path) -> None:
+        self.tile_image = pygame.image.load(f'assets/images/{tile_path}').convert_alpha()
 
-    def draw(self, screen, size, start_pos) -> None:
-        rows, cols = 10, 15
+    def draw(self, screen) -> None:
+        screen_width, screen_height = screen.get_size()
+        tile_width, tile_height = self.tile_image.get_size()
+        columns = screen_width // tile_width + 1
+        rows = screen_height // tile_height + 1
+
         for row in range(rows):
-            for col in range(cols):
-                screen.blit(self.tile_image, (start_pos[0] + col * size[0], start_pos[1] + row * size[1]))
+            for column in range(columns):
+                x = column * tile_width
+                y = row * tile_height
+                screen.blit(self.tile_image, (x, y))
+
+

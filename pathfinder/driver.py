d03a74cb80b42ffc41a4d23bc3e88c00ff6695f7
Megan <46995931+Chinosu@users.noreply.github.com>
2023-12-03 23:15:06 +1100

    Unify examples, add smooth plot

--- pathfinder/driver.py
+++ pathfinder/driver.py
@@ -1,5 +1,6 @@
-from pathfinder.astar import astar
-from pathfinder.plot import plot_path
+from astar import astar
+from plot import plot_path
+from smooth_plot import plot_smooth_path
 
 
 grid = [[0, 0, 0, 0, 1],
@@ -12,4 +13,5 @@ start = (0, 0)
 end = (4, 4)
 
 path = astar(grid, start, end)
-plot_path(grid, path, start, end)
+# plot_path(grid, path, start, end)
+plot_smooth_path(grid, path, start, end)

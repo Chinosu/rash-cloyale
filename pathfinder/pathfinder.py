050fa1efb953ea45fbaf69ce96850043c95a3780
Megan <46995931+Chinosu@users.noreply.github.com>
2023-12-04 19:24:33 +1100

    Comment out dev code

--- pathfinder/pathfinder.py
+++ pathfinder/pathfinder.py
@@ -1,17 +1,49 @@
 from astar import astar
-from plot import plot_path
-from smooth_plot import plot_smooth_path
+import plot
+import parse
 
 
-grid = [[0, 0, 0, 0, 1],
-        [1, 1, 0, 0, 0],
-        [0, 0, 0, 1, 0],
-        [0, 1, 1, 1, 0],
-        [0, 0, 0, 0, 0]]
+class Pathfinder:
+    def __init__(self, grid_str) -> None:
+        self.grid = parse.grid(grid_str)
+    
+    def update_grid(self, grid_str):
+        self.grid = parse.grid(grid_str)
 
-start = (0, 0)
-end = (4, 4)
+    def path(self, start, end):
+        path = astar(self.grid, start, end)
 
-path = astar(grid, start, end)
-# plot_path(grid, path, start, end)
-plot_smooth_path(grid, path, start, end)
+        self.last_start = start
+        self.last_end = end
+        self.last_path = path
+        return path
+
+    def print_grid(self):
+        raise NotImplemented()
+    
+    def print_path(self, smooth=True, path=None, start=None, end=None):
+        if path is None:
+            path = self.last_path
+        if start is None:
+            start = self.last_start
+        if end is None:
+            end = self.last_end
+
+        if smooth:
+            plot.smooth_path(self.grid, path, start, end)
+        else:
+            plot.path(self.grid, path, start, end)
+
+
+#  ------ DEMO ------ #
+def demo():
+    map = """
+    0  0  0  0  1
+    1  1  0  0  0
+    0  0  0  1  0
+    0  1  1  1  0
+    0  0  0  0  0
+    """
+    pathfinder = Pathfinder(map)
+    path = pathfinder.path((0, 0), (4, 4))
+    pathfinder.print_path()

2db3a5fd72c1357295e687666bcd0b30e8e980c3
Megan <46995931+Chinosu@users.noreply.github.com>
2023-12-04 19:01:42 +1100

    Add parse method

--- pathfinder/parse.py
+++ pathfinder/parse.py
@@ -44,6 +44,11 @@ big_str = """
 0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0
 """
 
+def parse_grid(str):
+    lines = [line.strip() for line in str.strip().split('\n') if line.strip()]
+    return [[int(number) for number in line.split()] for line in lines]
+
+
 lines = [line.strip() for line in big_str.strip().split('\n') if line.strip()]
 array_2d = [[int(number) for number in line.split()] for line in lines]
 

ab8d9185243e6dfd5f501cd5a3ca946dc3a556cf
Ren <142564582+Renostuten@users.noreply.github.com>
2023-12-06 22:10:09 +1100

    created basic layout including river, bridge and towers

--- map.py
+++ map.py
@@ -1,9 +1,28 @@
 import pygame
 
 class Map:
-    def __init__(self, bridge_img, river_img) -> None:
-        self.bridge = pygame.image.load(f'assets/images/{bridge_img}').convert_alpha()
-        self.river = pygame.image.load(f'assets/images/{river_img}').convert_alpha()
+    def __init__(self) -> None:
+        # To be used once assets have been found
+        # self.bridge = pygame.image.load(f'assets/images/{bridge_img}').convert_alpha()
+        # self.river = pygame.image.load(f'assets/images/{river_img}').convert_alpha()
+
+        self.bridge = pygame.Surface((75, 100))
+        self.bridge.fill('Brown')
+
+        self.river = pygame.Surface((1500, 50))
+        self.river.fill('Blue')
+
+        self.king = pygame.Surface((150, 150))
+        self.king.fill('Black')
+
+        self.princess = pygame.Surface((100, 100))
+        self.princess.fill('Black')
+
+        self.enemy_king = pygame.Surface((150, 150))
+        self.enemy_king.fill('Black')
+
+        self.enemy_princess = pygame.Surface((100, 100))
+        self.enemy_princess.fill('Black')
     
     def draw(self, screen):
         bridge_width, bridge_height = self.bridge.get_size()
@@ -17,3 +36,9 @@ class Map:
 
         screen.blit(self.river, (river_x, river_y))
         screen.blit(self.bridge, (bridge_x, bridge_y))
+
+        screen.blit(self.king, (100, 750))
+        screen.blit(self.princess, (1300, 600))
+
+        screen.blit(self.enemy_king, (1250, 100))
+        screen.blit(self.enemy_princess, (100, 300))
